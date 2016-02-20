ef6254498400ece716bc41c89d62e58d55941256
TomoyaHayakawa <simorgh3196@gmail.com>
2016-02-20 13:22:19 +0900

    first commit

---
+++ Twitter/app/src/main/java/com/project/simorgh/twitter/App.java
@@ -0,0 +1,34 @@
+package com.project.simorgh.twitter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+
+public class App extends Application {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "WGvM1fj40dRCaLeWVxLktJitz";
+    private static final String TWITTER_SECRET = "246jNuNDhWruphXeGyZYEHgvB43aRt5UrEevPvuzfo13fuTyYB";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.v("Override Application", "--- onCreate() in ---");
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        /** This Method Called when this Application finished. */
+        Log.v("Override Application", "--- onTerminate() in ---");
+    }
+}
---
+++ Twitter/app/src/main/java/com/project/simorgh/twitter/TweetComparator.java
@@ -0,0 +1,26 @@
+package com.project.simorgh.twitter;
+
+import com.twitter.sdk.android.core.models.Tweet;
+import java.util.Comparator;
+
+public class TweetComparator implements Comparator<Tweet> {
+
+        // 比較メソッド（データクラスを比較して-1, 0, 1を返すように記述する）
+        public int compare(Tweet a, Tweet b) {
+            long no1 = a.getId();
+            long no2 = b.getId();
+
+            // 降順でソートされる
+            if (no1 < no2) {
+                return 1;
+
+            } else if (no1 == no2) {
+                return 0;
+
+            } else {
+                return -1;
+
+            }
+        }
+
+}
